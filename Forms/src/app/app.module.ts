6d6bf93e48cb66a3990de80f402bb9256f9e457f
felipepatente <felipepatente@yahoo.com.br>
2021-03-23 19:52:57 -0300

    Route canDeactivate Guard

--- Forms/src/app/app.module.ts
+++ Forms/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.compo
 import { NavegacaoModule } from './navegacao/navegacao.module';
 import { AppRoutingModule } from './app.routes';
 import { AuthGuard } from './services/app.guard';
+import { CadastroGuard } from './services/cadastro.guard';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AuthGuard } from './services/app.guard';
     AppRoutingModule
   ],
   providers: [
-    AuthGuard
+    AuthGuard,
+    CadastroGuard
   ],
   bootstrap: [AppComponent]
 })
